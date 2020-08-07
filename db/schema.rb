8787e4503877453ee81c2cea6be37b87fc0dc8f3
wathigo <wathigosimon@gmail.com>
2020-08-07 19:01:18 +0300

    Migrate service, subject, and paper to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_140016) do
+ActiveRecord::Schema.define(version: 2020_08_07_155748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "papers", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name", null: false
---
+++ spec/factories/papers.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :paper do
+    name { "MyString" }
+  end
+end

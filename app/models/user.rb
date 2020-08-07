a6e3164152968ef0aa0b1f68d83dbf72a265bcba
wathigo <wathigosimon@gmail.com>
2020-08-07 17:01:09 +0300

    Add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+    enum role: %i[user writer admin].freeze
+    validates :email,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            presence: true,
+            uniqueness: { case_sensitive: false }
 end
